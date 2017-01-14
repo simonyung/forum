7f94d09a4e9cf66d1e93a6eb942cb67a9238f8a5
user <m134679258852@gmail.com>
2017-01-14 23:05:30 +0800

    Add authentication to application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @post = Post.all.order("created_at DESC")
@@ -10,7 +11,6 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post.user = current_user
     @post = current_user.posts.build
   end
 
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-       @post.destroy(post_params)
+       @post.destroy
       redirect_to root_path
   end
 
